16aea9d14672c0cf0aee1db4a1123d2b34db1a25
pataruco <pataruco@gmail.com>
2016-04-11 16:08:10 +0100

    Fix indentation

--- spec/githubUserSpec.js
+++ spec/githubUserSpec.js
@@ -15,30 +15,30 @@ describe('GitHubUser', function () {
   })
 
   describe("when parse GiHub API user data", function () {
-  gitHubUser.init(userData)
+    gitHubUser.init(userData)
 
-  it("return a username", function (done) {
-    expect(gitHubUser.username()).toBeDefined()
-    expect(gitHubUser.username()).toBe('pataruco')
-    done()
-  })
+    it("return a username", function (done) {
+      expect(gitHubUser.username()).toBeDefined()
+      expect(gitHubUser.username()).toBe('pataruco')
+      done()
+    })
 
-  it("return an image URL", function (done) {
-    expect(gitHubUser.imageUrl()).toBeDefined()
-    expect(gitHubUser.imageUrl()).toBe('https://avatars.githubusercontent.com/u/10330222?v=3')
-    done()
-  })
+    it("return an image URL", function (done) {
+      expect(gitHubUser.imageUrl()).toBeDefined()
+      expect(gitHubUser.imageUrl()).toBe('https://avatars.githubusercontent.com/u/10330222?v=3')
+      done()
+    })
 
-  it("return name of user", function (done) {
-    expect(gitHubUser.name()).toBeDefined()
-    expect(gitHubUser.name()).toBe('Pedro Martin')
-    done()
-  })
+    it("return name of user", function (done) {
+      expect(gitHubUser.name()).toBeDefined()
+      expect(gitHubUser.name()).toBe('Pedro Martin')
+      done()
+    })
 
-  it("return user page URL", function (done) {
-    expect(gitHubUser.pageUrl()).toBeDefined()
-    expect(gitHubUser.pageUrl()).toBe('https://github.com/pataruco')
-    done()
-  });
-}); // End of GitHub API user data
+    it("return user page URL", function (done) {
+      expect(gitHubUser.pageUrl()).toBeDefined()
+      expect(gitHubUser.pageUrl()).toBe('https://github.com/pataruco')
+      done()
+    });
+  }); // End of GitHub API user data
 })  // End of GitHubUser
