ca54eea02c1dcf180975d6b12c8eb0c4e157f283
pataruco <pataruco@gmail.com>
2016-04-11 16:02:54 +0100

    Test if object return a username

--- spec/githubUserSpec.js
+++ spec/githubUserSpec.js
@@ -1,4 +1,6 @@
 var GitHubUser = require('../scripts/github-user.js')
+var GitHubUser = require('../scripts/github-user.js'),
+      userData = require('./gitHubUserData.js')
 
 describe('GitHubUser', function () {
   var gitHubUser = new GitHubUser()
@@ -10,6 +12,15 @@ describe('GitHubUser', function () {
 
   it('be an object', function (done) {
     expect(typeof gitHubUser).toBe('object')
+    done()
+  })
+
+  describe("when parse GiHub API user data", function () {
+  gitHubUser.init(userData)
+
+  it("return a username", function (done) {
+    expect(gitHubUser.username()).toBeDefined()
+    expect(gitHubUser.username()).toBe('pataruco')
     done()
   })
 })  // End of GitHubUser
